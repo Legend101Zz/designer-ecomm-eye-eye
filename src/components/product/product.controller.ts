9a293c4ba50dea3c062ce4b8f3ca823b701604fd
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-09-04 10:34:40 +0530

    added product read feature

--- src/components/product/product.controller.ts
+++ src/components/product/product.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
-import { create } from '@components/product/product.service';
+import logger from '@core/utils/logger';
+import { create, read } from '@components/product/product.service';
 import { Iproduct } from './product.interface';
 
 const createProd = async (req: Request, res: Response) => {
@@ -10,5 +11,17 @@ const createProd = async (req: Request, res: Response) => {
   return res.send({ message: 'Created' });
 };
 
+const readProd = async (req: Request, res: Response) => {
+  try {
+    const prod = req.params.id;
+    const data = await read(prod);
+
+    return res.status(200).send({ message: 'success', data });
+  } catch (err) {
+    logger.error(err);
+    return res.status(501).send({ message: 'server error ' });
+  }
+};
+
 // eslint-disable-next-line import/prefer-default-export
-export { createProd };
+export { createProd, readProd };
--- src/components/product/product.router.ts
+++ src/components/product/product.router.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 import protectedByApiKey from '@core/middlewares/apiKey.middleware';
 import validation from '@core/middlewares/validate.middleware';
 import createProductValidation from './createProduct.validation';
-import { createProd } from './product.controller';
+import { createProd, readProd } from './product.controller';
 
 const router: Router = Router();
 
-// routes to be added
+// get routes
 
+router.get('/product/read/:id', [protectedByApiKey], readProd);
+
+// post routes
 router.post(
   '/product/create',
   [protectedByApiKey, validation(createProductValidation)],
