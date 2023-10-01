9fe82e4854be872b0518689b8988886b4e4f91cb
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-01 09:47:50 +0530

    added wishlist routers to api

--- src/api.ts
+++ src/api.ts
@@ -3,11 +3,13 @@ import healthCheck from '@components/healthcheck/healthCheck.router';
 import user from '@components/user/user.router';
 import product from '@components/product/product.router';
 import designer from '@components/designer/designer.router';
+import wishlist from '@components/wishlist/wishlist.router';
 
 const router: Router = Router();
 router.use(healthCheck);
 router.use(user);
 router.use(product);
 router.use(designer);
+router.use(wishlist);
 
 export default router;
