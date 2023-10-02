a006ac46b3344e68e56d39a65070981302ca6491
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-02 20:57:47 +0530

    added design router to the apis

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,7 @@ import user from '@components/user/user.router';
 import product from '@components/product/product.router';
 import designer from '@components/designer/designer.router';
 import wishlist from '@components/wishlist/wishlist.router';
+import design from '@components/design/design.router';
 
 const router: Router = Router();
 router.use(healthCheck);
@@ -11,5 +12,6 @@ router.use(user);
 router.use(product);
 router.use(designer);
 router.use(wishlist);
+router.use(design);
 
 export default router;
--- src/components/design/design.router.ts
+++ src/components/design/design.router.ts
@@ -4,11 +4,6 @@ import { showDesigns } from './design.controller';
 
 const router: Router = Router();
 
-router.get(
-  '/designs/show',
-  [protectedByApiKey],
-
-  showDesigns,
-);
+router.get('/designs/show', [protectedByApiKey], showDesigns);
 
 export default router;
