6e3f7c2ccb5a106fedf947bc5e127b3c64978171
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-11-01 19:25:13 +0530

    minor changes

--- src/components/design/design.router.ts
+++ src/components/design/design.router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import protectedByApiKey from '@core/middlewares/apiKey.middleware';
-import { showDesigns } from './design.controller';
+import { showDesigns, updateDesign } from './design.controller';
 
 const router: Router = Router();
 
 router.get('/designs/show', [protectedByApiKey], showDesigns);
+router.get('/designs/update/:designId', [protectedByApiKey], updateDesign);
 
 export default router;
